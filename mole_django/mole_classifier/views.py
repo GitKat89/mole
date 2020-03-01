b36e82ebe237a8a7709b35c4b15277705c53054b
Christine gessner <christine.gessner@gmx.net>
2020-03-01 23:20:24 +0100

    simple upload of text files works

--- mole_django/mole_classifier/views.py
+++ mole_django/mole_classifier/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.core.files.storage import FileSystemStorage
+#from .forms import UploadFileForm
 
 
 def index(request):
     return render(request, 'mole_classifier/index.html')
+
+def handle_uploaded_file(f):
+    print("entered handle_uploaded_file")
+    with open('/Users/deinemudda/Desktop/testupload.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+           destination.write(chunk)
+
+def upload(request):
+    print("UPLOAD DEF")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        
+  
+    return HttpResponseRedirect(reverse('index'))
+    
