f98abd9f82e2e363eed5c0354c2e8cb5c37e893b
Christine gessner <christine.gessner@gmx.net>
2020-03-01 18:34:25 +0100

    created index

--- mole_django/mole_django/settings.py
+++ mole_django/mole_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mole_classifier.apps.MoleClassifierConfig'
 ]
 
 MIDDLEWARE = [
