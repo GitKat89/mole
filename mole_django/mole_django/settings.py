9ef0e6e97843489f2441fcac503a5de99d0ba02e
finitystone <finitystone@gmail.com>
2020-03-15 22:30:13 +0100

    Set ALLOWED_HOSTS to all

--- mole_django/mole_django/settings.py
+++ mole_django/mole_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b+$_o&*(2*iw$8#de8epf=jjs$m*=&cc=&m#-kx3jv6hql^b#9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
